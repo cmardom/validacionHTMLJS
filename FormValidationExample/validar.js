6bf4e01e5f5d3f2a5f428d10389e43751bc7b9ed
carmen <cmardom599@g.educaand.es>
2023-10-27 13:03:31 +0200

    hello

---
+++ FormValidationExample/validar.js
@@ -0,0 +1,63 @@
+const formulario ;
+const nombre;
+const email;
+const edad;
+const boton;
+
+
+//PASO 1: Personalizar mensaje de validación
+//Indicar mayoría de edad
+function validaEdad() {
+
+}
+
+
+//Paso 2: Configurar mensaje la primera vez
+
+
+
+//PASO 3: Segunda validación tras la automática del navegador por HTML5
+// correo electrónico de dominio @gmail.com
+
+// function extraeDominio() { //Se usa en paso 3 y paso 5.
+  
+// }
+
+// function validaEmail(){
+
+// }
+
+// function validaFormulario(event) {
+
+// }
+
+
+
+
+//PASO 4: Eliminar validación automática del navegador
+
+
+
+//PASO 5: Añadir validación únicamente Javascript (Requisito paso 4)
+
+// function validaNombre() {
+
+// }
+// function validaEmail(){
+
+// }
+
+// function validaEdadHTML5(){
+
+// }
+
+// function validaEdadJS(){
+
+// }
+
+// function validaFormulario(event) {
+
+// }
+
+
+
